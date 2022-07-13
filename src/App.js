fe0fbe0020ff5c51320eb1115269789a4884cf77
Mariya Pastushonok <mariyayaromchik@mail.ru>
2022-07-13 21:46:53 +0300

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
-import Categories from './components/Categories';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import PizzaBlock from './components/PizzaBlock';
-import Sort from './components/Sort';
+import { Cart } from './pages/Cart';
+import Main from './pages/Main';
+import NotFound from './pages/NotFound';
 import './scss/app.scss';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 
 function App() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    axios.get('https://62cef9f4826a88972d06f45e.mockapi.io/items')
-    .then((res) => setItems(res.data))
-  }, [])
 
   return (
     <div className="wrapper">
       <Header />
     <div className="content">
       <div className="container">
-        <div className="content__top">
-          <Categories />
-          <Sort />
-        </div>
-        <h2 className="content__title">Все пиццы</h2>
-        <div className="content__items">
-          {
-            items.map(({title, price, imageUrl, sizes, types, id}) => (
-              <PizzaBlock  
-                key={id}
-                title={title} 
-                price={price} 
-                image={imageUrl}
-                sizes={sizes}
-                type={types}
-              />
-            ))
-          }
-        </div>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   </div>
---
+++ src/pages/Main.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PizzaBlock from '../components/PizzaBlock';
+import Sort from '../components/Sort';
+import Categories from '../components/Categories';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Main = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://62cef9f4826a88972d06f45e.mockapi.io/items')
+    .then((res) => setItems(res.data))
+  }, [])
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {
+          items.map(({title, price, imageUrl, sizes, types, id}) => (
+            <PizzaBlock  
+              key={id}
+              title={title} 
+              price={price} 
+              image={imageUrl}
+              sizes={sizes}
+              type={types}
+            />
+          ))
+        }
+      </div>
+    </>
+  )
+}
+
+export default Main;
